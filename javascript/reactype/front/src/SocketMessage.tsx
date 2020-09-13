8418157feff88f57503c76340b02e48f2acdccaf
bauworks <pub001@bauworks.jp>
2020-09-14 00:18:41 +0900

    軽微な修正

--- javascript/reactype/front/src/SocketMessage.tsx
+++ javascript/reactype/front/src/SocketMessage.tsx
@@ -1,41 +1,56 @@
-import React, * as react from "react";
+import React from "react";
 
 interface ISocketMessageState {
-    endpoint: string
-    messages: Array<string>
+  endpoint: string;
+  messages: Array<string>;
 }
 
-class SocketMessage extends react.Component{   
-    state : ISocketMessageState
+class SocketMessage extends React.Component {
+  state: ISocketMessageState;
 
-    constructor(props: any){
-      super(props);
-      this.state={
-        endpoint:"ws://localhost:9999",
-        messages:[]
-      }
-    }
-  
-    componentDidMount(){
-      //initialize connection
-      const ws = new WebSocket(this.state.endpoint)
-      //ws.onopen = () =>{
-      //  //send any msg from Client if needed
-      //  ws.send(JSON.stringify(temp))
-      //}
-      //save whatever response from client
-      ws.onmessage = evt =>{
-        this.setState({
-          messages:this.state.messages.concat(evt.data)
-        })
-      }
-    }
-    render(){
-      return(
-        <div>
-          <p>{this.state.messages.map((items: React.ReactNode)=><li>{items}</li>)}</p>
-        </div>
-    )}
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      endpoint: "ws://localhost:9999",
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    // initialize connection
+    const ws = new WebSocket(this.state.endpoint);
+    // ws.onopen = () =>{
+    //  //send any msg from Client if needed
+    //  ws.send(JSON.stringify(temp))
+    // }
+    // save whatever response from client
+    ws.onmessage = (evt) => {
+      this.setState({
+        messages: this.state.messages.concat(evt.data),
+      });
+    };
+  }
+
+  componentWillUnmount() {
+    this.setState({
+      endpoint: "",
+      messages: [],
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+          {this.state.messages.map((items: React.ReactNode, index: number) => (
+            <li key="index">
+              {index} : {items}
+            </li>
+          ))}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default SocketMessage;

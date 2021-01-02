867e5e8928b2192a8f9a5815551971cae2fb86b5
bauworks <pub001@bauworks.jp>
2021-01-02 16:17:39 +0900

    C#開発環境検証

---
+++ csharp/helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello-BAUWORKS!!!!");
+        }
+    }
+}
---
+++ csharp/sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+        }
+
+        static int SayHello()
+        {
+            Console.WriteLine("Hello BAUWORKS!");
+            return 0;
+        }
+    }
+}

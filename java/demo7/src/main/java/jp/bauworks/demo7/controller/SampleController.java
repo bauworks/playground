9e9c785234efa3aaa616924de7b8694a97012080
BAUWORKS <pub001@bauworks.jp>
2021-05-08 19:00:06 +0900

    パッケージを修正

--- java/demo7/src/main/java/jp/bauworks/demo7/controller/SampleController.java
+++ java/demo7/src/main/java/jp/bauworks/demo7/controller/SampleController.java
@@ -1,4 +1,4 @@
-package jp.bauworks.demo7;
+package jp.bauworks.demo7.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

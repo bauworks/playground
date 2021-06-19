57eab291234fb1744a2846b91e91b29ab6156d33
BAUWORKS <pub001@bauworks.jp>
2021-06-19 18:24:51 +0900

    init TetirsApp

---
+++ javascript/react-tetris/reactapp/src/TetrisApp/TetrisApp.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+//***********************************
+// Tetris APPコンポーネント
+//***********************************
+export const TetrisApp:React.FC = () => {
+
+  return (
+    <div className="App">
+      Hello, Tetris APP.
+    </div>
+  );
+}
---
+++ javascript/react-tetris/reactapp/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {TetrisApp} from './TetrisApp/TetrisApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TetrisApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);

c70e7e6286234cbc04ecd680e113a85bcea0ac20
BAUWORKS <pub001@bauworks.jp>
2021-07-03 21:16:32 +0900

    テトリスの動きが完成（リファクタリング前）

--- javascript/react-tetris/reactapp/src/Molecules/TilesBoard.tsx
+++ javascript/react-tetris/reactapp/src/Molecules/TilesBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback, useMemo } from 'react'
 import {Tile} from '../Atoms/Tile';
 
 
@@ -25,6 +25,16 @@ const toColor = (code: number) : string => {
       return "white";
   };
 };
+
+//***********************************
+// ブロック種類をランダムに返す
+//***********************************
+const getRandomBlock = () : number =>   Math.floor(Math.random() * (6+1) +1);
+
+//***********************************
+// ブロックの回転をランダムに返す
+//***********************************
+const getRandomRotate = () : number =>  Math.floor(Math.random() * (3+1));
 
 //***********************************
 // ブロックのパターン
@@ -88,7 +98,35 @@ const blockPatterns: number[][][][] = [
   ]
 ]
 
+//***********************************
+// ブロックを置けるかチェックする
+//***********************************
+const checkPutBlock = (
+  props: putBlockProps,
+  allTiles:　number[][]) :boolean => {
 
+    const x: number = props.x;
+    const y: number = props.y;
+    const block: number = props.block;
+    const rotate: number = props.rotate;
+  
+    if (
+      y + blockPatterns[block][rotate][0][1] > allTiles.length -1 ||
+      y + blockPatterns[block][rotate][1][1] > allTiles.length -1 ||
+      y + blockPatterns[block][rotate][2][1] > allTiles.length -1 ||
+      y + blockPatterns[block][rotate][3][1] > allTiles.length -1 ||
+      x + blockPatterns[block][rotate][0][0] > allTiles[0].length -1 ||
+      x + blockPatterns[block][rotate][1][0] > allTiles[0].length -1 ||
+      x + blockPatterns[block][rotate][2][0] > allTiles[0].length -1 ||
+      x + blockPatterns[block][rotate][3][0] > allTiles[0].length -1 ||
+      allTiles[ y + blockPatterns[block][rotate][0][1] ][ x + blockPatterns[block][rotate][0][0] ] !== 0 ||
+      allTiles[ y + blockPatterns[block][rotate][1][1] ][ x + blockPatterns[block][rotate][1][0] ] !== 0 ||
+      allTiles[ y + blockPatterns[block][rotate][2][1] ][ x + blockPatterns[block][rotate][2][0] ] !== 0 ||
+      allTiles[ y + blockPatterns[block][rotate][3][1] ][ x + blockPatterns[block][rotate][3][0] ] !== 0 ){
+        return false;
+    }
+    return true;
+}
 //***********************************
 // ブロックを指定座標にセットする関数
 //***********************************
@@ -111,56 +149,28 @@ const putBlock = (props: putBlockProps,
   const bf_block: number = bfProps.block;
   const bf_rotate: number = bfProps.rotate;
 
-  console.log('putBlock()開始');
-  console.log(bf_x + ", " + bf_y + ", " + bf_block + ", " + bf_rotate);
-  console.log(x + ", " + y + ", " + block + ", " + rotate);
-
   if ( bf_x >= 0 ){
-    console.log('前回セットしたブロックをクリアする');
-
-    console.log('0:[' + (y + blockPatterns[block][rotate][0][0]) + '][' + (x + blockPatterns[block][rotate][0][1]) + ']');
-    console.log('1:[' + (y + blockPatterns[block][rotate][1][0]) + '][' + (x + blockPatterns[block][rotate][1][1]) + ']');
-    console.log('2:[' + (y + blockPatterns[block][rotate][2][0]) + '][' + (x + blockPatterns[block][rotate][2][1]) + ']');
-    console.log('3:[' + (y + blockPatterns[block][rotate][3][0]) + '][' + (x + blockPatterns[block][rotate][3][1]) + ']');
-
     // 前回セットしたブロックをクリアする
-    allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][0][0] ][ bf_x + blockPatterns[bf_block][bf_rotate][0][1] ] = 0;
-    allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][1][0] ][ bf_x + blockPatterns[bf_block][bf_rotate][1][1] ] = 0;
-    allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][2][0] ][ bf_x + blockPatterns[bf_block][bf_rotate][2][1] ] = 0;
-    allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][3][0] ][ bf_x + blockPatterns[bf_block][bf_rotate][3][1] ] = 0;
+    allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][0][1] ][ bf_x + blockPatterns[bf_block][bf_rotate][0][0] ] = 0;
+    allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][1][1] ][ bf_x + blockPatterns[bf_block][bf_rotate][1][0] ] = 0;
+    allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][2][1] ][ bf_x + blockPatterns[bf_block][bf_rotate][2][0] ] = 0;
+    allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][3][1] ][ bf_x + blockPatterns[bf_block][bf_rotate][3][0] ] = 0;
 
-    console.log('新しくブロックをセットして良いかチェックする');
-    // 新しくブロックをセットして良いかチェックする
-    if (
-        y + blockPatterns[block][rotate][0][0] > allTiles.length -1 ||
-        y + blockPatterns[block][rotate][1][0] > allTiles.length -1 ||
-        y + blockPatterns[block][rotate][2][0] > allTiles.length -1 ||
-        y + blockPatterns[block][rotate][3][0] > allTiles.length -1 ||
-        x + blockPatterns[block][rotate][0][1] > allTiles[0].length -1 ||
-        x + blockPatterns[block][rotate][1][1] > allTiles[0].length -1 ||
-        x + blockPatterns[block][rotate][2][1] > allTiles[0].length -1 ||
-        x + blockPatterns[block][rotate][3][1] > allTiles[0].length -1 ||
-        allTiles[ y + blockPatterns[block][rotate][0][0] ][ x + blockPatterns[block][rotate][0][1] ] !== 0 ||
-        allTiles[ y + blockPatterns[block][rotate][1][0] ][ x + blockPatterns[block][rotate][1][1] ] !== 0 ||
-        allTiles[ y + blockPatterns[block][rotate][2][0] ][ x + blockPatterns[block][rotate][2][1] ] !== 0 ||
-        allTiles[ y + blockPatterns[block][rotate][3][0] ][ x + blockPatterns[block][rotate][3][1] ] !== 0 ){
-
-          console.log('チェックNG => 前回セットしたブロックを元に戻す');
+    if ( checkPutBlock(props, allTiles) === false ) {
           // 前回セットしたブロックを元に戻す
-          allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][0][0] ][ bf_x + blockPatterns[bf_block][bf_rotate][0][1] ] = bf_block;
-          allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][1][0] ][ bf_x + blockPatterns[bf_block][bf_rotate][1][1] ] = bf_block;
-          allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][2][0] ][ bf_x + blockPatterns[bf_block][bf_rotate][2][1] ] = bf_block;
-          allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][3][0] ][ bf_x + blockPatterns[bf_block][bf_rotate][3][1] ] = bf_block;
+          allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][0][1] ][ bf_x + blockPatterns[bf_block][bf_rotate][0][0] ] = bf_block;
+          allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][1][1] ][ bf_x + blockPatterns[bf_block][bf_rotate][1][0] ] = bf_block;
+          allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][2][1] ][ bf_x + blockPatterns[bf_block][bf_rotate][2][0] ] = bf_block;
+          allTiles[ bf_y + blockPatterns[bf_block][bf_rotate][3][1] ][ bf_x + blockPatterns[bf_block][bf_rotate][3][0] ] = bf_block;
           return false;
     }
   }
 
-  console.log('新しくブロックをセットする');
   // 新しくブロックをセットする
-  allTiles[ y + blockPatterns[block][rotate][0][0] ][ x + blockPatterns[block][rotate][0][1] ] = block;
-  allTiles[ y + blockPatterns[block][rotate][1][0] ][ x + blockPatterns[block][rotate][1][1] ] = block;
-  allTiles[ y + blockPatterns[block][rotate][2][0] ][ x + blockPatterns[block][rotate][2][1] ] = block;
-  allTiles[ y + blockPatterns[block][rotate][3][0] ][ x + blockPatterns[block][rotate][3][1] ] = block;
+  allTiles[ y + blockPatterns[block][rotate][0][1] ][ x + blockPatterns[block][rotate][0][0] ] = block;
+  allTiles[ y + blockPatterns[block][rotate][1][1] ][ x + blockPatterns[block][rotate][1][0] ] = block;
+  allTiles[ y + blockPatterns[block][rotate][2][1] ][ x + blockPatterns[block][rotate][2][0] ] = block;
+  allTiles[ y + blockPatterns[block][rotate][3][1] ][ x + blockPatterns[block][rotate][3][0] ] = block;
 
   return true;
 }
@@ -181,9 +191,7 @@ export const TilesBoard: React.FC<Props> = (props: Props) => {
     /*
      * 全タイル情報
      */
-
     const initialTilesInfo = () => {
-      console.log('initialTilesInfo() Start');
       const infos: number[][] = new Array<number[]>(props.height);
       for (let i=0; i < infos.length; i++){
         infos[i] = new Array<number>(props.width).fill(0);
@@ -196,70 +204,163 @@ export const TilesBoard: React.FC<Props> = (props: Props) => {
     /*
      * 前回のタイル更新Props
      */
-    const [bfProps, setBfProps] = useState<putBlockProps>({x:-1, y:-1, block:-1, rotate:-1});
+    const [bfProps, setBfProps] = useState<putBlockProps>({
+      x:-1,
+      y:-1,
+      block:getRandomBlock(),
+      rotate:getRandomRotate()
+    });
 
 
     /*
-     * 移動後のブロック位置
+     * ブロックの初期位置
      */
-    const [nextPos, setNextPos] = useState<number[]>([6, 2]);
+    const firstPos = useMemo(() => [Math.floor(props.width/2), 1], [props.width]);
+
+    /*
+     * 次回ブロック位置
+     */
+    const [nextPos, setNextPos] = useState<number[]>([firstPos[0], firstPos[1]]);
+
+
+    /*
+     * 次回ブロック回転角度
+     */
+    const [nextRotate, setNextRotate] = useState<number>(0);
 
 
     //-------------------------------
     // タイルを並べる(列)
     //-------------------------------
-    const tilesRow = (rowInfo: number[]) => (rowInfo.map((info) => 
-      <Tile bgcolor={toColor(info)} />
+    const tilesRow = (rowInfo: number[]) => (rowInfo.map((info, index) => 
+      <Tile bgcolor={toColor(info)} key={index}/>
     ));
 
     //-------------------------------
     // タイルを並べる(行)
     //-------------------------------
     const tilesAll = tilesInfo.map((rowInfo, index) => 
-      <div>
+      <div key={index}>
         {tilesRow(rowInfo)}
       </div>
     );
 
 
+    //-------------------------------
+    // イベント処理：キー入力
+    //-------------------------------
+    const keyDown = useCallback((event) => {
+      // console.log("Key Pressed : " + event.keyCode );
+      if (event.keyCode === 32) {         // Space
+        setNextRotate(angle => angle === 3 ? 0 : angle + 1);
+      }
+      else if (event.keyCode === 90) {    // z
+        setNextRotate(angle => angle === 3 ? 0 : angle + 1);
+      }
+      else if (event.keyCode === 88) {    // x
+        setNextRotate(angle => angle === 0 ? 3 : angle - 1);
+      }
+      else if (event.keyCode === 37) {    // Left
+        setNextPos(xy => [xy[0]-1, xy[1]]);
+      }
+      else if (event.keyCode === 38) {    // Up
+        setNextPos(xy => [xy[0], xy[1]-1]);
+      }
+      else if (event.keyCode === 39) {    // Right
+        setNextPos(xy => [xy[0]+1, xy[1]]);
+      }
+      else if (event.keyCode === 40) {    // Down
+        setNextPos(xy => [xy[0], xy[1]+1]);
+      }
+    }, []);
+
+    useEffect(() => {
+      document.addEventListener("keydown", keyDown, false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     //-------------------------------
-    // ブロック移動指示
+    // イベント処理：タイマー
     //-------------------------------
-    // ブロックを下に落とす
+    // ブロック降下
     const downBlock = () => {
       setNextPos(xy => [xy[0], xy[1]+1]);
     };
 
+
+    //-------------------------------
+    // ブロック移動メイン
+    //-------------------------------
     useEffect(() => {
+
+      let newTilesInfo = tilesInfo;
+
       // Props編集
       const props :putBlockProps = {
         x: nextPos[0],
         y: nextPos[1],
-        block: Math.floor( Math.random() * (6+1) )+1,
-        rotate: Math.floor( Math.random() * (3+1) )
+        block: bfProps.block,
+        rotate: nextRotate
       }
-      // ブロックをセットする
-console.log('ブロックをセットする]');
-      const ret = putBlock(props, bfProps, tilesInfo);
+    
+      // ブロックを次の位置にセットする
+      const ret = putBlock(props, bfProps, newTilesInfo);
 
       if (ret === true) {
-        // ブロックの移動が成功の場合は、Propsを前回値として保存
+        // ブロックの移動が成功の場合、Propsを前回値として保存
         setBfProps(props);
       } else {
-        // ブロックの移動が失敗の場合は、nextPosを前回値に戻す
-        setNextPos([bfProps.x, bfProps.y]);
+        // 下移動で失敗した場合は、次のブロックを出現させる
+        if (props.y > bfProps.y) {
+
+          // １列揃った行があればその列を削除する
+          newTilesInfo = newTilesInfo.filter((rowInfo) => {
+            return !(rowInfo.length === rowInfo.filter((info)=>info !== 0).length)
+          });
+
+          // 削除されて不足した列を挿入
+          for (let rows = newTilesInfo.length; rows < tilesInfo.length; rows++) {
+            const infos: number[] = new Array<number>(tilesInfo[0].length).fill(0);
+            newTilesInfo.unshift(infos);
+          }
+
+          bfProps.x = firstPos[0];
+          bfProps.y = firstPos[1];
+          bfProps.block = getRandomBlock();
+          bfProps.rotate = getRandomRotate();
+
+          // ブロックを置けるかチェック
+          if (checkPutBlock(bfProps, newTilesInfo)){
+            // チェックOK
+            setBfProps(bfProps);
+            setNextPos([bfProps.x, bfProps.y]);
+          } else {
+            // チェックNG => ゲームオーバー
+            console.log('ゲームオーバー!!!!!!!!');
+            clearInterval(timerId);
+          }
+        } else {
+          // その他移動で失敗した場合は、nextPos, nextRotateを前回値に戻す
+          setNextPos([bfProps.x, bfProps.y]);
+          setNextRotate(bfProps.rotate);
+        }
+
       }
 
       // タイル情報を更新
-      setTilesInfo(tilesInfo);
+      setTilesInfo(newTilesInfo);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [nextPos]);
+    }, [nextPos, nextRotate]);
   
+    //------------------------
     // タイマー処理（ブロック降下）
+    //------------------------
+    const [timerId, setTimerId] = useState<number>(0);
+
     useEffect(() => {
-      const timerId = setInterval(downBlock, 1000);
+      const timerId = window.setInterval(downBlock, 1000);
+      setTimerId(timerId);
       return () => clearInterval(timerId);
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
@@ -270,8 +371,6 @@ console.log('ブロックをセットする]');
     //------------------------
     // タイルボード
     const tileBoardStyle: React.CSSProperties = {
-      width: "400px",
-      height: "400px",
       display: "table"
     };
 
